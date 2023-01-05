3629348469df341b1351bfd09df737628a76e915
Jayant Deol <jayantdeol5@gmail.com>
2023-01-05 11:07:20 +0530

    Create middle_linked_list.cpp

---
+++ Linked List/middle_linked_list.cpp
@@ -0,0 +1,30 @@
+// APPROACH 1
+class Solution
+{
+public:
+    int getLength(ListNode *head)
+    {
+        int len = 0;
+        while (head != NULL)
+        {
+            len++;
+            head = head->next;
+        }
+        return len;
+    }
+
+    ListNode *middleNode(ListNode *head)
+    {
+        int len = getLength(head);
+        int ans = (len / 2);
+
+        ListNode *temp = head;
+        int cnt = 0;
+        while (cnt < ans)
+        {
+            temp = temp->next;
+            cnt++;
+        }
+        return temp;
+    }
+};
