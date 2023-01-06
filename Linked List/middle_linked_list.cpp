123fb62c3a7cd2e9cffc25fb5e87ebed71add73f
Jayant Deol <jayantdeol5@gmail.com>
2023-01-06 22:19:07 +0530

    Update middle_linked_list.cpp

--- Linked List/middle_linked_list.cpp
+++ Linked List/middle_linked_list.cpp
@@ -28,3 +28,33 @@ public:
         return temp;
     }
 };
+
+class Solution
+{
+public:
+    int getLength(ListNode *head)
+    {
+        int len = 0;
+        while (head != NULL)
+        {
+            len++;
+            head = head->next;
+        }
+        return len;
+    }
+
+    ListNode *middleNode(ListNode *head)
+    {
+        int len = getLength(head);
+        int ans = (len / 2);
+
+        ListNode *temp = head;
+        int cnt = 0;
+        while (cnt < ans)
+        {
+            temp = temp->next;
+            cnt++;
+        }
+        return temp;
+    }
+};
