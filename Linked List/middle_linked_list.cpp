f9b1287ddeaf8e84075e29dcd6160d1ea03a0436
Jayant Deol <jayantdeol5@gmail.com>
2023-01-07 16:27:25 +0530

    Update middle_linked_list.cpp

--- Linked List/middle_linked_list.cpp
+++ Linked List/middle_linked_list.cpp
@@ -29,32 +29,40 @@ public:
     }
 };
 
+// APPROACH 2
 class Solution
 {
 public:
-    int getLength(ListNode *head)
+    ListNode *getMiddle(ListNode *head)
     {
-        int len = 0;
-        while (head != NULL)
+
+        if (head == NULL || head->next == NULL)
         {
-            len++;
-            head = head->next;
+            return head;
         }
-        return len;
+
+        if (head->next->next == NULL)
+        {
+            return head->next;
+        }
+
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+
+        while (fast != NULL)
+        {
+            fast = fast->next;
+            if (fast != NULL)
+            {
+                fast = fast->next;
+            }
+            slow = slow->next;
+        }
+        return slow;
     }
 
     ListNode *middleNode(ListNode *head)
     {
-        int len = getLength(head);
-        int ans = (len / 2);
-
-        ListNode *temp = head;
-        int cnt = 0;
-        while (cnt < ans)
-        {
-            temp = temp->next;
-            cnt++;
-        }
-        return temp;
+        return getMiddle(head);
     }
 };
